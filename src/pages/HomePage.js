e3a9b9939592970a3ee99881ffc4f178e88a3ee4
Harsh Varshney <harsh@Harshs-MacBook-Air.local>
2023-02-12 19:24:32 +0530

    ani fixed

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import { FeaturedProducts, Hero, Services, Contact } from '../components'
 const HomePage = () => {
-  return <main>
+  return <motion.main
+  initial={{opacity: 0}}
+  animate={{opacity: 1}}
+  exit={{opacity: 0}}>
    <Hero></Hero>
    <FeaturedProducts></FeaturedProducts>
    <Services></Services>
    <Contact></Contact>
-  </main>
+  </motion.main>
 }
 
 export default HomePage
