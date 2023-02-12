d29817f2117f4ec12e2c1461f4d8375a62b3b060
divyam <divyammishra0918@gmail.com>
2023-02-13 00:42:46 +0530

    image order fixed

--- src/utils/cat.js
+++ src/utils/cat.js
@@ -1,5 +1,5 @@
 export const cat = [
     { id: 1, name: 'image1', image: 'https://i.ibb.co/fFf62HW/Artboard-3.png',price: '100' },
-    { id: 2, name: 'image2', image: 'https://i.ibb.co/M9RgHpg/Artboard-3-copy-2.png',price: '200' },
-    { id: 3, name: 'image3', image: 'https://i.ibb.co/ZgvQK8n/Artboard-3-copy.png',price: '500' },
-];
+    { id: 2, name: 'image2', image: 'https://i.ibb.co/ZgvQK8n/Artboard-3-copy.png',price: '200' },
+    { id: 3, name: 'image3', image:'https://i.ibb.co/M9RgHpg/Artboard-3-copy-2.png' ,price: '500' },
+]; 
