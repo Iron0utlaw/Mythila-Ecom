c0d1d7e4c5f1ce0d46b3fe9161ba218a8fd9f729
Harsh Varshney <harsh@Harshs-MacBook-Air.local>
2023-02-12 13:49:13 +0530

    Router setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import { Navbar, Sidebar, Footer } from './components'
+import styled from 'styled-components'
+
+import {Home,Products,SingleProduct,Error,About,Cart,Checkout,PrivateRoute} from './pages'
 
 function App() {
-  return <h4>comfy sloth starter</h4>
+  return <div>
+    <Router>
+      <Navbar/>
+      <Sidebar/>
+      <Switch>
+        <Route exact path='/'> <Home/> </Route>
+        <Route exact path='/about'> <About/> </Route>
+        <Route exact path='/cart'> <Cart/> </Route>
+        <Route exact path='/checkout'> <Checkout/> </Route>
+        <Route exact path='/products'> <Products/> </Route>
+        <Route exact path='/products/:id' children={<SingleProduct/>}/> 
+        <Route exact path='*'> <Error/> </Route>
+      </Switch>
+      <Footer/>
+    </Router>
+  </div>
 }
 
 export default App
---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+import Home from './HomePage'
+import Products from './ProductsPage'
+import SingleProduct from './SingleProductPage'
+import Error from './ErrorPage'
+import About from './AboutPage'
+import Cart from './CartPage'
+import Checkout from './CheckoutPage'
+import PrivateRoute from './PrivateRoute'
+
+export{Home,Products,SingleProduct,Error,About,Cart,Checkout,PrivateRoute}
