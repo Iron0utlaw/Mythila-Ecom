51ef32e439562441d0151aa496a6b308c2ab21cc
divyam <divyammishra0918@gmail.com>
2023-02-15 18:01:57 +0530

    services done

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -11,10 +11,10 @@ const Hero = () => {
       <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries</p>
       <Link to='/products' className='btn hero-btn'>Shop Now</Link>
     </article>
-<articlr className='img-container'>
-  <img src={heroBcg} className="main-img"></img>
-  <img src={heroBcg2} className="accent-img"></img>
-</articlr>
+<article className='img-container'>
+  <img src={heroBcg} className="main-img" alt='banner-img'></img>
+  <img src={heroBcg2} className="accent-img" alt='banner-support-img'></img>
+</article>
   </Wrapper>
 }
 
--- src/components/Services.js
+++ src/components/Services.js
@@ -3,7 +3,25 @@ import styled from 'styled-components'
 import { services } from '../utils/constants'
 
 const Services = () => {
-  return <h4>services </h4>
+  return <Wrapper>
+    <div className='section-center'>
+      <article className='header'>
+        <h3>custom paintings <br/>built only for you</h3>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries</p>
+      </article>
+      <div className='services-center'>
+        {services.map((service)=>{
+           return <article key={service.id} className="service">
+            <span className='icon'>{service.icon}</span>
+            <h4>{service.title}</h4>
+            <p>{service.text}</p>
+           </article>
+
+        })}
+      </div>
+    </div>
+  </Wrapper>
+
 }
 
 const Wrapper = styled.section`
