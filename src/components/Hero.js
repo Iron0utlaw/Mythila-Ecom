3e2e891d07a42a08ab6fbc2c1f4348fb64f52fa4
Harsh Varshney <harsh@Harshs-MacBook-Air.local>
2023-02-16 23:43:41 +0530

    Spell check

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -7,7 +7,7 @@ import heroBcg2 from '../assets/Artboard2.png'
 const Hero = () => {
   return <Wrapper className='section-center'>
     <article className='content'>
-      <h1>Desgin your <br/>comfort zone</h1>
+      <h1>Design your <br/>comfort zone</h1>
       <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries</p>
       <Link to='/products' className='btn hero-btn'>Shop Now</Link>
     </article>
