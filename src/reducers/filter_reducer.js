64d331514e1e84b3a571752750bd732a521549a1
divyam <divyammishra0918@gmail.com>
2023-03-19 15:28:29 +0530

    grid and list function done

--- src/reducers/filter_reducer.js
+++ src/reducers/filter_reducer.js
@@ -1,3 +1,4 @@
+import { act } from 'react-dom/test-utils';
 import {
   LOAD_PRODUCTS,
   SET_LISTVIEW,
@@ -13,6 +14,12 @@ const filter_reducer = (state, action) => {
   if(action.type===LOAD_PRODUCTS){
     return {...state,all_products:[...action.payload],filtered_products:[...action.payload],};
   }
+  if (action.type===SET_GRIDVIEW){
+return {...state,grid_view:true}
+  }
+  if (action.type===SET_LISTVIEW){
+    return {...state,grid_view:false}
+      }
   throw new Error(`No Matching "${action.type}" - action type`)
 }
 
